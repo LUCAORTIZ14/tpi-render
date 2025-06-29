bceaf861597257ba9b9597d2a49685e6b26fba4c
Carlos Salgado <128559215+carlossalgado1@users.noreply.github.com>
2025-06-29 16:17:34 -0300

    Update and rename ventas.py to reservas.py

---
+++ models/reservas.py
@@ -0,0 +1,13 @@
+from config.database import Base
+from sqlalchemy import Column, Integer, Boolean, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+
+class Reservas(Base):
+
+    __tablename__ = "reservas"
+    
+    id= Column(Integer, primary_key=True)
+    vehiculo_id = (Integer, ForeignKey('vehiculo.id'), nullable=False)
+    usuario_id=Column(Integer, ForeignKey('usuarios.id'), nullable=False)
+    fecha_reserva = Column(DateTime) 
+    fecha_devolucion = Column(DateTime)
