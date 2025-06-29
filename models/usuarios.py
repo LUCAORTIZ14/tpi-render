fa6318698d7ddb8e3a33398c9d8b4185ea5da6ca
Carlos Salgado <128559215+carlossalgado1@users.noreply.github.com>
2025-06-29 16:01:14 -0300

    Update usuarios.py

--- models/usuarios.py
+++ models/usuarios.py
@@ -6,15 +6,7 @@ class Usuarios(Base):
     __tablename__ = "usuarios"
 
     id = Column(Integer, primary_key = True)
-    apellido = Column(String(20))
     nombre= Column(String(20))
     correo= Column(String(100))
-    password=Column(String(1000))
-    avatar=Column(Text)
-    pais= Column(String(50)) 
-    ciudad= Column(String(50))  
-    direccion= Column(String(50)) 
-    telefono= Column(String(20)) 
-    role= Column(String(50)) 
-
-   
+    contraseña=Column(String(1000))
+    rol= Column(String(50)) 
