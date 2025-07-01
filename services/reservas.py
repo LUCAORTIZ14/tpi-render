a9e6616d11b07946c079d6c57bca9f899369fbfa
Carlos Salgado <128559215+carlossalgado1@users.noreply.github.com>
2025-07-01 19:21:43 -0300

    Update reservas.py

--- services/reservas.py
+++ services/reservas.py
@@ -13,10 +13,24 @@ class ReservaService():
         return self.db.query(ReservaModel).filter(ReservaModel.id == id).first()
 
     def create_reserva(self, reserva: Reserva):
+        # Verificar si ya hay una reserva superpuesta para el mismo vehículo
+        reservas_conflictivas = (
+            self.db.query(ReservaModel)
+            .filter(ReservaModel.vehiculo_id == reserva.vehiculo_id)
+            .filter(ReservaModel.fecha_reserva <= reserva.fecha_devolucion)
+            .filter(ReservaModel.fecha_devolucion >= reserva.fecha_reserva)
+            .all()
+        )
+
+        if reservas_conflictivas:
+            raise Exception("El vehículo ya está reservado en esas fechas")
+
+        # Si no hay conflicto, se guarda la reserva
         new_reserva = ReservaModel(**reserva.dict())
         self.db.add(new_reserva)
         self.db.commit()
         return
+
 
     def delete_reserva(self, id: int):
         self.db.query(ReservaModel).filter(ReservaModel.id == id).delete()
