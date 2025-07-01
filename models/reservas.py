f9bf60bcd9fb66e2b3f833c6a263cf93044c69e0
Carlos Salgado <128559215+carlossalgado1@users.noreply.github.com>
2025-07-01 16:19:47 -0300

    Update reservas.py

--- models/reservas.py
+++ models/reservas.py
@@ -1,13 +1,11 @@
 from config.database import Base
-from sqlalchemy import Column, Integer, Boolean, ForeignKey, DateTime
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, ForeignKey, DateTime
 
 class Reserva(Base):
-
     __tablename__ = "reservas"
-    
-    id= Column(Integer, primary_key=True)
+
+    id = Column(Integer, primary_key=True)
     vehiculo_id = Column(Integer, ForeignKey('vehiculos.id'), nullable=False)
-    usuario_id=Column(Integer, ForeignKey('usuarios.id'), nullable=False)
-    fecha_reserva = Column(DateTime) 
+    usuario_id = Column(Integer, ForeignKey('usuarios.id'), nullable=False)
+    fecha_reserva = Column(DateTime)
     fecha_devolucion = Column(DateTime)
