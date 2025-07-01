7496f40418f99260683803adef170fab60748778
Carlos Salgado <128559215+carlossalgado1@users.noreply.github.com>
2025-07-01 16:20:33 -0300

    Update reservas.py

--- schemas/reservas.py
+++ schemas/reservas.py
@@ -1,11 +1,13 @@
-from pydantic import BaseModel, Field, EmailStr
-from typing import Optional, List
+from pydantic import BaseModel, Field
+from typing import Optional
 from datetime import datetime
 
 class Reserva(BaseModel):
     id: Optional[int] = None
-    vehiculo_id : int
-    usuario_id: int 
-    fecha_reserva : datetime
-    fecha_devolucion : datetime
+    vehiculo_id: int
+    usuario_id: int
+    fecha_reserva: datetime
+    fecha_devolucion: datetime
 
+    class Config:
+        from_attributes = True  # Para usar .from_orm()
