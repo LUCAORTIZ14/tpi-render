7e888e7688188c6476b5405abf8b233da2a8fe63
Joaquin Pedraza <103124128+J0AQUINPEDRAZA@users.noreply.github.com>
2025-07-01 01:17:26 -0300

    Update jwt_manager.py

--- utils/jwt_manager.py
+++ utils/jwt_manager.py
@@ -1,9 +1,20 @@
-from jwt import encode, decode
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+
+SECRET_KEY = "my_secret_key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 def create_token(data: dict) -> str:
-    token: str = encode(payload=data, key="my_secret_key", algorithm="HS256")
-    return token
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 def validate_token(token: str) -> dict:
-    data: dict = decode(token, key="my_secret_key", algorithms=['HS256'])
-    return data
+    try:
+        decoded = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return decoded
+    except JWTError:
+        return {}
