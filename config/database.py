ab3628f0319356f65e750c31ba22e07285e56b95
LUCAORTIZ14 <lucaortiz31@gmail.com>
2025-07-02 19:29:58 -0300

    Update database.py

--- config/database.py
+++ config/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL ="mysql+mysqlconnector://root:123456@localhost:3306/tienda2"
+DATABASE_URL ="postgresql://tpifinal_user:EEIj7Il5QO3FWtK5aYPliR3khs6CsFU3@dpg-d1hkhj3uibrs73fc8a80-a/tpifinal"
 #"postgresql://tpifinal_user:EEIj7Il5QO3FWtK5aYPliR3khs6CsFU3@dpg-d1hkhj3uibrs73fc8a80-a/tpifinal"
 
 #"postgresql://tpidb:q1cPUwbJSXGkp0rnTvhwIzyIitcsKcOg@dpg-d1glg7idbo4c73avrtn0-a/tpidb"
