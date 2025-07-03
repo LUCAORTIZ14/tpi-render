0291251e58fbc91fe320587dff4645b8441b8de8
LUCAORTIZ14 <lucaortiz31@gmail.com>
2025-07-02 21:06:34 -0300

    modificaciones en reserva

--- models/reservas.py
+++ models/reservas.py
@@ -1,5 +1,5 @@
 from config.database import Base
-from sqlalchemy import Column, Integer, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, ForeignKey
 
 class Reserva(Base):
     __tablename__ = "reservas"
@@ -7,5 +7,5 @@ class Reserva(Base):
     id = Column(Integer, primary_key=True)
     vehiculo_id = Column(Integer, ForeignKey('vehiculos.id'), nullable=False)
     usuario_id = Column(Integer, ForeignKey('usuarios.id'), nullable=False)
-    fecha_reserva = Column(DateTime)
-    fecha_devolucion = Column(DateTime)
+    fecha_reserva = Column(str)
+    fecha_devolucion = Column(str)
--- schemas/reservas.py
+++ schemas/reservas.py
@@ -1,13 +1,12 @@
 from pydantic import BaseModel, Field
 from typing import Optional
-from datetime import datetime
 
 class Reserva(BaseModel):
     id: Optional[int] = None
     vehiculo_id: int
     usuario_id: int
-    fecha_reserva: datetime
-    fecha_devolucion: datetime
+    fecha_reserva: str
+    fecha_devolucion: str
 
     class Config:
         from_attributes = True  # Para usar .from_orm()
